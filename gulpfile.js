e09b26ef3042dbf96a5edeeb801678810b0a4693
Evgen Makhnonosov <evgen.makhnonosov@.nixsolutions.com>
2017-11-17 18:01:23 +0200

    Updated gulp task.

--- gulpfile.js
+++ gulpfile.js
@@ -1,28 +1,15 @@
 var gulp = require('gulp');
 
 const paths = {
-    js : 'src/main/resources/static/js/**/*.js',
-    css : 'src/main/resources/static/css/**/*.css',
-    html : 'src/main/resources/static/**/*.html',
-    targetJs : 'target/classes/static/js',
-    targetCss : 'target/classes/static/css',
-    targetHtml : 'target/classes/static/',
+    src : 'src/main/resources/static/**/*.*',
+    target : 'target/classes/static/',
 };
 
 gulp.task('watch', function(){
-    gulp.watch(paths.js, ['devJs']);
-    gulp.watch(paths.css, ['devCss']);
-    gulp.watch(paths.html, ['devHtml']);
+    gulp.watch(paths.src, ['dev']);
 });
 
-gulp.task('devJs', function() {
-    return gulp.src(paths.js).pipe(gulp.dest(paths.targetJs));
-});
 
-gulp.task('devCss', function() {
-    return gulp.src(paths.css).pipe(gulp.dest(paths.targetCss));
-});
-
-gulp.task('devHtml', function() {
-    return gulp.src(paths.html).pipe(gulp.dest(paths.targetHtml));
+gulp.task('dev', function() {
+    return gulp.src(paths.src).pipe(gulp.dest(paths.target));
 });
